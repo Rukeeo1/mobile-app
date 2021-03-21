0c8aaf58a1bdeeab02580e6b85808ec0e518a1be
jagajastic <josephibrahi@gmail.com>
2021-03-21 16:48:24 +0100

    add crop card, button, and flesh up styles

--- src/screens/Crops/CropSelection.jsx
+++ src/screens/Crops/CropSelection.jsx
@@ -1,13 +1,13 @@
-import { AntDesign } from '@expo/vector-icons'
+import { AntDesign, Entypo, MaterialIcons } from '@expo/vector-icons'
 import { LinearGradient } from 'expo-linear-gradient'
-import React, { useState } from 'react'
+import React from 'react'
 import {
-  Image,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
+    Image,
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native'
 import { Input } from '../../components/'
 import { GradientButton } from '../../components/Button'
@@ -16,7 +16,7 @@ import constants from '../../constants'
 const { colors } = constants
 const CropSelection = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: 'dodgerblue' }}>
+    <View style={{ flex: 1, backgroundColor: colors.white }}>
       <SafeAreaView>
         <ScrollView>
           <View>
@@ -24,15 +24,158 @@ const CropSelection = () => {
               style={[styles.searchContainer]}
               colors={[colors.green, colors.greenDeep]}
             >
+              <AntDesign name="left" size={24} color={colors.white} />
+              <View style={{ alignItems: 'center' }}>
+                <Text style={[styles.titleTop]}>Tomato</Text>
+
+                <View style={[styles.titleTag]}>
+                  <MaterialIcons name="star" size={20} color={colors.white} />
+                  <Text style={{ color: colors.white, marginHorizontal: 4 }}>
+                    Begginer star
+                  </Text>
+                </View>
+              </View>
               <View>
                 <Input
                   placeholder="Search"
                   containerStyle={styles.searchInputContainer}
                   inputStyle={{ marginTop: -10, paddingRight: 10 }}
-                >
-                </Input>
+                ></Input>
+              </View>
+
+              <View style={{ alignItems: 'center', marginVertical: 15 }}>
+                <Text style={{ color: colors.white, marginVertical: 4 }}>
+                  You can find this on your seed pack
+                </Text>
+
+                <Text style={{ fontWeight: 'bold', color: colors.white }}>
+                  What type is the variety you have chosen
+                </Text>
+              </View>
+
+              <View>
+                <GradientButton gradient={[colors.blueLigth, colors.blue]}>
+                  <View
+                    style={{
+                      alignItems: 'center',
+                      width: '100%',
+                      paddingHorizontal: 20,
+                    }}
+                  >
+                    <Text style={[styles.btnText]}>Vine (corbon)</Text>
+                  </View>
+                </GradientButton>
+                <GradientButton gradient={[colors.blueLigth, colors.blue]}>
+                  <View
+                    style={{
+                      alignItems: 'center',
+                      width: '100%',
+                      paddingHorizontal: 20,
+                    }}
+                  >
+                    <Text style={[styles.btnText]}>Bush (Trailing)</Text>
+                  </View>
+                </GradientButton>
               </View>
             </LinearGradient>
+          </View>
+
+          <View style={{ alignItems: 'center', marginVertical: 20 }}>
+            <Text style={{ fontSize: 20 }}>Grow it Recommends</Text>
+            <Text style={{ width: '80%', textAlign: 'center' }}>
+              Still need to buy seeds? Here is some inspiration for you with
+              tried and tested suggestions.
+            </Text>
+          </View>
+
+          <View style={[styles.cropSection]}>
+            <View style={[styles.cropCardContainer]}>
+              <View style={[styles.cropDetails]}>
+                <Image
+                  style={[styles.cropAvatar]}
+                  source={require('../../assets/avatarimg.png')}
+                />
+                <View style={[styles.cropText]}>
+                  <Text style={[styles.cropName]}>Tomato</Text>
+                  <Text>Intermediate</Text>
+                </View>
+              </View>
+              <AntDesign name="right" size={24} color={colors.green} />
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                width: 200,
+                paddingLeft: 20,
+                paddingRight: 20,
+              }}
+            >
+              <GradientButton gradient={[colors.blueLigth, colors.blue]}>
+                <View
+                  style={{
+                    alignItems: 'center',
+                    width: '100%',
+                    paddingHorizontal: 20,
+                  }}
+                >
+                  <Text style={[styles.btnText]}>Grow It</Text>
+                </View>
+              </GradientButton>
+              <GradientButton gradient={[colors.blueLigth, colors.blue]}>
+                <View
+                  style={{
+                      width: '100%',
+                      flexDirection: 'row',
+                      justifyContent: 'space-around',
+                    alignItems: 'center',
+                    paddingHorizontal: 20,
+                  }}
+                >
+                  <Text style={[styles.btnText]}>Buy seed</Text>
+                  <Entypo name="shopping-cart" size={24} color={colors.white} />
+                </View>
+              </GradientButton>
+            </View>
+            <View style={[styles.cropCardContainer]}>
+              <View style={[styles.cropDetails]}>
+                <Image
+                  style={[styles.cropAvatar]}
+                  source={require('../../assets/avatarimg.png')}
+                />
+                <View style={[styles.cropText]}>
+                  <Text style={[styles.cropName]}>Tomato</Text>
+                  <Text>Intermediate</Text>
+                </View>
+              </View>
+              <AntDesign name="right" size={24} color={colors.green} />
+            </View>
+            <View style={[styles.cropCardContainer]}>
+              <View style={[styles.cropDetails]}>
+                <Image
+                  style={[styles.cropAvatar]}
+                  source={require('../../assets/avatarimg.png')}
+                />
+                <View style={[styles.cropText]}>
+                  <Text style={[styles.cropName]}>Tomato</Text>
+                  <Text>Intermediate</Text>
+                </View>
+              </View>
+              <AntDesign name="right" size={24} color={colors.green} />
+            </View>
+            <View style={[styles.cropCardContainer]}>
+              <View style={[styles.cropDetails]}>
+                <Image
+                  style={[styles.cropAvatar]}
+                  source={require('../../assets/avatarimg.png')}
+                />
+                <View style={[styles.cropText]}>
+                  <Text style={[styles.cropName]}>Tomato</Text>
+                  <Text>Intermediate</Text>
+                </View>
+              </View>
+              <AntDesign name="right" size={24} color={colors.green} />
+            </View>
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -42,10 +185,16 @@ const CropSelection = () => {
 
 const styles = StyleSheet.create({
   searchContainer: {
-    paddingTop: '20%',
+    paddingTop: '10%',
     paddingBottom: '10%',
     paddingHorizontal: 25,
   },
+  titleTop: {
+    fontSize: 42,
+    color: colors.white,
+    marginTop: 5,
+  },
+  titleTag: { flexDirection: 'row', alignItems: 'center', marginBottom: 30 },
   searchForm: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -60,21 +209,29 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   cropSection: {
-    marginVertical: 28,
-    padding: 20,
+    marginVertical: 5,
+    paddingHorizontal: 20,
   },
   cropCardContainer: {
+    backgroundColor: colors.white,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     width: '95%',
     borderRadius: 100 / 2,
-    borderColor: colors.greyDark,
-    borderWidth: 1,
-    borderColor: '#ddd',
     height: 70,
     paddingRight: 20,
-    marginVertical: 3,
+    marginVertical: 5,
+    // shadow iOS
+    shadowColor: 'grey',
+    shadowOffset: {
+      width: 0.5,
+      height: 0.4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    // shadow android
+    elevation: 15,
   },
   cropDetails: {
     flexDirection: 'row',
@@ -91,21 +248,9 @@ const styles = StyleSheet.create({
   cropName: {
     fontSize: 20,
   },
-  arrow: {
-    width: 20,
-    height: 20,
-  },
   btnText: {
     color: colors.white,
     fontWeight: 'bold',
-  },
-  gradientBtn: {
-    marginVertical: 50,
-  },
-  createNewCropBtn: {
-    padding: 20,
-    flex: 1,
-    bottom: '-30%',
   },
 })
 
