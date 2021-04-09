0ac27c9956ed172fbefbf7cf5869741a5ec5d171
jagajastic <josephibrahi@gmail.com>
2021-04-09 08:25:11 +0100

    add follwers screen

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import Following from './Following';
+import Followers from './Followers';
 import ProfileMain from './ProfileMain';
 import ProfileSettings from './Settings';
 import GrowItCalendar from './AddToCalendar'
@@ -20,6 +21,7 @@ const ProfileNavigator = () => {
       <Screen name='Profile-Settings' component={ProfileSettings} />
       <Screen name='Main-Profile' component={ProfileMain} />
       <Screen name='Following' component={Following} />
+      <Screen name='Followers' component={Followers} />
       <Screen name='GrowItCalendar' component={GrowItCalendar} />
     </Navigator>
   );
