07915e074e80fb08a782bb8d9b2d638b454cc893
Rukee Ojigbo <rukeeojigbo@gmail.com>
2021-04-11 12:51:02 +0100

    chore: add font family

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { useFonts } from 'expo-font';
 
 import RootNavigator from './src/navigation';
 
-import { initializeFirebase } from './src/config/';
+import store from './src/redux';
 
-initializeFirebase();
+// import { initializeFirebase } from './src/config/';
+
+// initializeFirebase();
+
+const customFonts = {
+  'Hero-New-Regular': require('./src/assets/fonts/Hero-New-Regular-Italic.otf'),
+  'Hero-New-Regular-Italic': require('./src/assets/fonts/Hero-New-Regular.otf'),
+};
 
 export default function App() {
+  const [fontsLoaded] = useFonts(customFonts);
   return (
-    <View style={styles.container}>
-      <RootNavigator />
-    </View>
+    fontsLoaded && (
+      <Provider store={store}>
+        <View style={styles.container}>
+          <RootNavigator />
+        </View>
+      </Provider>
+    )
   );
 }
 
---
+++ src/redux/actions/loadingActions.js
@@ -0,0 +1,7 @@
+import { LOADING } from '../types';
+
+export const handleLoading = () => ({
+  type: LOADING,
+});
+
+export default handleLoading;
