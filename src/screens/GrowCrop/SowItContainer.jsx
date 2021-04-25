cb4c0ff417fdf3b2699a8996a7916c889a35da17
Rukee Ojigbo <rukeeojigbo@gmail.com>
2021-04-25 14:27:16 +0100

    chore: clean ups

--- src/screens/GrowCrop/SowItContainer.jsx
+++ src/screens/GrowCrop/SowItContainer.jsx
@@ -10,7 +10,14 @@ import constants from '../../constants';
 
 const { colors } = constants;
 
-export const SowItContainer = ({ buttonTitle, tip }) => {
+export const SowItContainer = ({
+  buttonTitle,
+  tip,
+  renderIcon,
+  reminderText,
+  showHoriazontalButtonAfterDateIsSelected,
+  onPressOfHorizontalBtn,
+}) => {
   const [showSowItButton, setShowSowItButton] = useState(true);
   const [showCalender, setShowCalender] = useState(false);
   const [showFullSelectedDate, setShowFullSelectedDate] = useState(false);
@@ -19,34 +26,34 @@ export const SowItContainer = ({ buttonTitle, tip }) => {
   const [selectedMonth, setSelectedMonth] = useState('February');
   const [selectedYear, setSelectedYear] = useState('2021');
 
-  const renderConfirmIcon = (callBack) => {
-    return (
-      <View
-        style={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          flex: '1',
-          paddingHorizontal: '5%',
-          marginRight: 20,
-          marginTop: '2%',
-        }}
-      >
-        <TouchableOpacity
-          onPress={callBack}
-          style={{
-            height: 50,
-            width: 50,
-            borderRadius: 25,
-            backgroundColor: colors.pink,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <AntDesign name='right' size={29} color={colors.white} />
-        </TouchableOpacity>
-      </View>
-    );
-  };
+  // const renderConfirmIcon = (callBack) => {
+  //   return (
+  //     <View
+  //       style={{
+  //         alignItems: 'center',
+  //         justifyContent: 'center',
+  //         flex: '1',
+  //         paddingHorizontal: '5%',
+  //         marginRight: 20,
+  //         marginTop: '2%',
+  //       }}
+  //     >
+  //       <TouchableOpacity
+  //         onPress={callBack}
+  //         style={{
+  //           height: 50,
+  //           width: 50,
+  //           borderRadius: 25,
+  //           backgroundColor: colors.pink,
+  //           justifyContent: 'center',
+  //           alignItems: 'center',
+  //         }}
+  //       >
+  //         <AntDesign name='right' size={29} color={colors.white} />
+  //       </TouchableOpacity>
+  //     </View>
+  //   );
+  // };
 
   return (
     <View>
@@ -78,65 +85,78 @@ export const SowItContainer = ({ buttonTitle, tip }) => {
               setShowSowItButton(false);
               setShowFullSelectedDate(true);
             }}
-            renderIcon={renderConfirmIcon}
+            // renderIcon={renderConfirmIcon}
+            renderIcon={renderIcon}
           />
         </View>
       )}
       {showFullSelectedDate && (
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
+        <>
           <View
             style={{
-              justifyContent: 'center',
-              paddingHorizontal: '6%',
-              borderTopLeftRadius: 45,
-              borderBottomLeftRadius: 45,
-              borderTopRightRadius: 45,
-              marginVertical: '5%',
-              borderBottomRightRadius: 45,
-              backgroundColor: 'white',
-              width: '80%',
-              // height: 60,
-              shadowColor: 'grey',
-              shadowOffset: {
-                width: 0.5,
-                height: 0.4,
-              },
-              shadowOpacity: 0.3,
-              shadowRadius: 4,
-              elevation: 15,
-              // paddingVertical: '5%',
-              paddingVertical: '3%',
-            }}
-          >
-            <Text>Reminder to sow</Text>
-            <Text
-              style={{ color: colors.pink, fontSize: 18, marginTop: '2%' }}
-            >{`${selectedDate} ${selectedMonth} ${selectedYear}`}</Text>
-          </View>
-          <TouchableOpacity
-            onPress={() => {
-              setShowCalender(true);
-              setShowSowItButton(false);
-              setShowFullSelectedDate(false);
-            }}
-            style={{
-              height: 50,
-              width: 50,
-              borderRadius: 25,
-              backgroundColor: colors.pink,
-              justifyContent: 'center',
+              flexDirection: 'row',
               alignItems: 'center',
+              justifyContent: 'space-between',
             }}
           >
-            <Feather name='clock' size={24} color={colors.white} />
-          </TouchableOpacity>
-        </View>
+            <View
+              style={{
+                justifyContent: 'center',
+                paddingHorizontal: '6%',
+                borderTopLeftRadius: 45,
+                borderBottomLeftRadius: 45,
+                borderTopRightRadius: 45,
+                marginTop: '5%',
+                borderBottomRightRadius: 45,
+                backgroundColor: 'white',
+                flex: 1,
+                shadowColor: 'grey',
+                shadowOffset: {
+                  width: 0.5,
+                  height: 0.4,
+                },
+                shadowOpacity: 0.3,
+                shadowRadius: 4,
+                elevation: 15,
+                paddingVertical: '3%',
+                marginRight: !showHoriazontalButtonAfterDateIsSelected ? 20 : 0,
+              }}
+            >
+              <Text fontType='light'>{reminderText}</Text>
+              <Text
+                style={{ color: colors.pink, fontSize: 18, marginTop: '2%' }}
+                fontType='light'
+              >{`${selectedDate} ${selectedMonth} ${selectedYear}`}</Text>
+            </View>
+            {!showHoriazontalButtonAfterDateIsSelected && (
+              <TouchableOpacity
+                onPress={() => {
+                  setShowCalender(true);
+                  setShowSowItButton(false);
+                  setShowFullSelectedDate(false);
+                }}
+                style={{
+                  height: 50,
+                  width: 50,
+                  borderRadius: 25,
+                  backgroundColor: colors.pink,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginTop: 15,
+                }}
+              >
+                <Feather name='clock' size={24} color={colors.white} />
+              </TouchableOpacity>
+            )}
+          </View>
+          {showHoriazontalButtonAfterDateIsSelected && (
+            <Button
+              title='End Harvest'
+              gradient={[colors.pink, colors.pinkDeep]}
+              onPress={onPressOfHorizontalBtn ? onPressOfHorizontalBtn : null}
+            />
+          )}
+        </>
       )}
     </View>
   );
