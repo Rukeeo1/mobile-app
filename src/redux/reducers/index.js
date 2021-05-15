313b46b15492e3b28f9c5a197bd14f9526a8fcfc
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2021-05-15 16:13:35 +0100

    feature(explore): Explore screen API integration

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import loading from './loadingReducer';
 import auth from './authReducer';
 import crops from './cropReducer';
+import posts from './postsReducer'
 
 export default combineReducers({
   loading,
   auth,
   crops,
+  posts,
 });
